4d3cd29d95a46e744b613ea13c110e77469c61a9
Jason-Donmoyer <j.donmoyer523@gmail.com>
2021-10-13 18:17:57 -0400

    HTML and CSS: -HTML: --removed hard coded quotes and authors JS NOT CSS!: --added call to api to fetch quotes and populate UI

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-console.log('Logging it');
+// Variables
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const author = document.getElementById('author');
+const twitterButton = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote');
+
+
+// Global Variable to Hold Quote Arrays
+let apiQuotes = [];
+
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // Check if author field exists
+  quote.author ? author.textContent = quote.author : author.textContent = 'Unknown';
+  // Check if text length is longer than...
+  quote.text.length > 120 ? quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote');
+  quoteText.textContent = quote.text;
+}
+
+// Get Quotes From API
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+    console.log(error);
+  }
+}
+
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${author.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+
+// Event Listeners
+newQuoteButton.addEventListener('click', newQuote);
+twitterButton.addEventListener('click', tweetQuote);
+
+// On Load
+getQuotes();
